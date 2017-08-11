0ef90e71d60b50361a4b6ea67acf764ce31bfe31
Antoine Bichon <antoine.bichon@cgi.com>
2017-08-11 14:41:24 +0200

    Const & enums

--- TEDExporter/TedToLeaKitMapper.cs
+++ TEDExporter/TedToLeaKitMapper.cs
@@ -4,69 +4,85 @@ namespace TEDExporter
 {
     public static class TedToLeaKitMapper
     {
+        public static string CARD_TYPE_TACHES = "512063320";
+        public static string CARD_TYPE_EVOL = "512063321";
+        public static string CARD_TYPE_SUIVI = "519685034";
+
+        public static string LANE_SUIVI = "521748899";
+        public static string LANE_EVOL_ESRC = "521517640";
+        public static string LANE_EVOL_TAURI = "516686007";
+        public static string LANE_EVOL_FSS = "516686010";
+
+        public static string LANE_TACHE_ESRC = "512726405";
+        public static string LANE_TACHE_TAURI = "512720054";
+        public static string LANE_TACHE_FSS = "512720055";
+
+        public static string LANE_IMPREVU_ESRC = "512726407";
+        public static string LANE_IMPREVU_TAURI = "512720056";
+        public static string LANE_IMPREVU_FSS = "512720057";
 
         public static ImportLeanKit GetDTOForTED(ExportTED ted)
         {
             var t = new ImportLeanKit();
             t.Card_Description = ted.Intitule;
-            t.Card_Title = "TED " + ted.Numero;
+            t.Card_Title = $"TED {ted.Numero}";
             var domaine = GetDomaine(ted);
             var nature = GetNature(ted);
             switch (nature)
             {
-                case "SuiviProd":
-                    t.Lane_Id = "521748899";
-                    t.Card_Type = "519685034";
+                case NatureTED.Suivi_Production:
+                    t.Lane_Id = LANE_SUIVI;
+                    t.Card_Type = CARD_TYPE_SUIVI;
                     break;
-                case "Evol":
-                    t.Card_Type = "512063321";
+                case NatureTED.Evolution:
+                    t.Card_Type = CARD_TYPE_EVOL;
                     switch (domaine)
                     {
                        
-                        case "eSRC":
-                            t.Lane_Id = "521517640";
+                        case DomaineTED.eSRC:
+                            t.Lane_Id = LANE_EVOL_ESRC;
                             break;
-                        case "Tauri":
-                            t.Lane_Id = "516686007";
+                        case DomaineTED.Tauri:
+                            t.Lane_Id = LANE_EVOL_TAURI;
                             break;
-                        case "FSS":
+                        case DomaineTED.FSS:
                         default:
-                            t.Lane_Id = "516686010";
+                            t.Lane_Id = LANE_EVOL_FSS;
                             break;
                     }
                     break;
-                case "Tache":
-                    t.Card_Type = "512063320";
+                case NatureTED.Tache:
+                    t.Card_Type = CARD_TYPE_TACHES;
                     switch (domaine)
                     {
                        
-                        case "eSRC":
-                            t.Lane_Id = "512726405";
+                        case DomaineTED.eSRC:
+                            t.Lane_Id = LANE_TACHE_ESRC;
                             break;
-                        case "Tauri":
-                            t.Lane_Id = "512720054";
+                        case DomaineTED.Tauri:
+                            t.Lane_Id = LANE_TACHE_TAURI;
                             break;
-                        case "FSS":
+                        case DomaineTED.FSS:
                         default:
-                            t.Lane_Id = "512720055";
+                            t.Lane_Id = LANE_TACHE_FSS;
                             break;
                     }
                     break;
-                case "Imprevu":
+                case NatureTED.Imprevu:
                 default:
-                    t.Card_Type = "512063321";
+                    t.Card_Type = CARD_TYPE_TACHES;
                     switch (domaine)
                     {
                         
-                        case "eSRC":
-                            t.Lane_Id = "512726407";
+                        case DomaineTED.eSRC:
+                            t.Lane_Id = LANE_IMPREVU_ESRC;
                             break;
-                        case "Tauri":
-                            t.Lane_Id = "512720056";
+                        case DomaineTED.Tauri:
+                            t.Lane_Id = LANE_IMPREVU_TAURI;
                             break;
-                        case "FSS":
+                        case DomaineTED.FSS:
                         default:
-                            t.Lane_Id = "512720057";
+                            t.Lane_Id = LANE_IMPREVU_FSS;
                             break;
                     }
                     break;
@@ -77,49 +93,67 @@ namespace TEDExporter
         }
 
 
-        public static string GetDomaine(ExportTED ted)
+        public static DomaineTED GetDomaine(ExportTED ted)
         {
             if (ted.SousSysteme == "Prestations FSS")
             {
-                return "FSS";
+                return DomaineTED.FSS;
             }
             if(ted.SousSysteme == "eSRC" || ted.Intitule.ToUpper().StartsWith("ESRC"))
             {
-                return "eSRC";
+                return DomaineTED.eSRC;
             }
             if(ted.SousSysteme == "ServicesCommuns")
             {
-                return "commun";
+                return  DomaineTED.Commun;
             }
             if (ted.SousSysteme.StartsWith("Actuaria"))
             {
-                return "Tauri";
+                return DomaineTED.Tauri;
             }
-            return string.Empty;
+            return DomaineTED.Inconnu;
 
         }
 
-        public static string GetNature(ExportTED ted)
+        public static NatureTED GetNature(ExportTED ted)
         {
             if (ted.Intitule.ToUpper().StartsWith("[SUIVI") || ted.Intitule.ToUpper().Contains("SUIVI DE PROD"))
             {
-                return "SuiviProd";
+                return NatureTED.Suivi_Production;
             }
             if(ted.Type == "Evolution")
             {
-                return "Evol";
+                return NatureTED.Evolution;
             }
 
             if(ted.Type == "Imprévu")
             {
-                return "Imprevu";
+                return NatureTED.Imprevu;
             }
 
             if(ted.Type == "Tâche")
             {
-                return "Tache";
+                return NatureTED.Tache;
             }
-            return string.Empty;
+            return NatureTED.Inconnu;
         }
+    }
+
+    public enum DomaineTED
+    {
+        FSS,
+        eSRC,
+        Commun,
+        Tauri,
+        Inconnu
+    }
+
+    public enum NatureTED
+    {
+        Suivi_Production,
+        Evolution,
+        Imprevu,
+        Tache,
+        Inconnu
     }
 }
