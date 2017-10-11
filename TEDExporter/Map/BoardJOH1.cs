284f6f9b02801ac506d696c65a7890f89046aedb
ErriBrr <rer-b@laposte.net>
2017-10-11 17:51:04 +0200

    v0.5

---
+++ TEDExporter/Map/BoardJOH1.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEDExporter.Map
+{
+    public class BoardJOH1 : Board
+    {
+        public const int ID = 547360586;
+
+        public const int ID_IMPREVU = 547775053;
+        public const int ID_DEFAUT = 547775057;
+        public const int ID_TACHE = 547775061;
+        public const int ID_EVOLUTION = 547775065;
+        public const int ID_SUIVI = 547775069;
+
+        public const string TITRE_IMPREVU = "backlog:ted imprÃ©vus";
+        public const string TITRE_DEFAUT = "backlog:tfs";
+        public const string TITRE_TACHE = "backlog:ted tÃ¢ches";
+        public const string TITRE_EVOLUTION = "backlog:ted evolutions";
+        public const string TITRE_SUIVI = "backlog:ted suivi production";
+    }
+}
