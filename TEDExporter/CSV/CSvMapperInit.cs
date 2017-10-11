6c0e669caae7b95fde430280473709293020ea54
ErriBrr <rer-b@laposte.net>
2017-10-11 17:28:57 +0200

    0.5

--- TEDExporter/CSV/CSvMapperInit.cs
+++ TEDExporter/CSV/CSvMapperInit.cs
@@ -1,1 +1,15 @@
 
+using CsvHelper.Configuration;
+using TEDExporter.DTO;
+
+namespace TEDExporter
+{
+    public class CSvMapperInit : CsvClassMap<ExportInit>
+    {
+
+        public CSvMapperInit()
+        {
+            Map(m => m.Numero).Name("Numero");
+        }
+    }
+}
