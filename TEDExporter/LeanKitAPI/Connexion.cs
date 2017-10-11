d607d2f9728ce0540f9b57bf711d8318a2a61699
ErriBrr <rer-b@laposte.net>
2017-10-11 17:31:56 +0200

    v0.5

---
+++ TEDExporter/LeanKitAPI/Connexion.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEDExporter.LeanKitAPI
+{
+    internal static class Connexion
+    {
+        internal const string hostName = [your hostname in leankit];
+        internal const string emailAddress = [your email in leankit];
+        internal const string password = [your pwd in leankit];
+    }
+}
